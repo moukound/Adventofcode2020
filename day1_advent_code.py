e909140df79566beaf163d2a4a02aac7e4bdf136
Gopal Mukunda (PS/ESY-CVE) <Mukunda.Gopal@de.bosch.com>
2020-12-29 19:02:15 +0100

    commit 3. Break statements added.

--- day1_advent_code.py
+++ day1_advent_code.py
@@ -1,5 +1,6 @@
 
 from numpy import loadtxt
+endloop = 0
 expense_report = loadtxt("day1_Expense.txt", unpack=False)
 len_expense = len(expense_report)
 print ("length is %s " % len_expense)
@@ -9,13 +10,16 @@ for i in (range(len_expense)):
     for j in range(len_expense):
         y = expense_report[j]
         sum_now = x+y
-        print(sum_now)
         if (sum_now == 2020.0):
             product = x*y
             print(product)
+            endloop = 1
+    if(endloop == 1):
         break
 
+
 #part 2
+endloop = 0
 expense_report = loadtxt("day1_Expense.txt", unpack=False)
 len_expense = len(expense_report)
 print ("length is %s " % len_expense)
@@ -25,9 +29,13 @@ for i in (range(len_expense)):
         k = j+1
         for k in range(len_expense):
             if(expense_report[i]+expense_report[j]+expense_report[k] == 2020):
-                print("answer found")
                 print(expense_report[i]*expense_report[j]*expense_report[k])
-                break
+                endloop = 1
+        if (endloop == 1):
+            break
+    if (endloop == 1):
+        break
+
 
 
 
