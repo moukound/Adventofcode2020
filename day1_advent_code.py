b6c697c0883c785a6f514ae774694d7923c77fa3
Gopal Mukunda (PS/ESY-CVE) <Mukunda.Gopal@de.bosch.com>
2020-12-29 17:34:03 +0100

    Commit 2 :-) With Karthik on the line

--- day1_advent_code.py
+++ day1_advent_code.py
@@ -1,15 +1,34 @@
+
 from numpy import loadtxt
-expense_report = loadtxt("C:\\CVE\\Python_stuff\\Advent_Of_Code\\day1_Expense.txt", unpack=False)
+expense_report = loadtxt("day1_Expense.txt", unpack=False)
+len_expense = len(expense_report)
+print ("length is %s " % len_expense)
+# part 1
+for i in (range(len_expense)):
+    x = expense_report[i]
+    for j in range(len_expense):
+        y = expense_report[j]
+        sum_now = x+y
+        print(sum_now)
+        if (sum_now == 2020.0):
+            print('answer found')
+            product = x*y
+            print(product)
+        break
+
+#part 2
+expense_report = loadtxt("day1_Expense.txt", unpack=False)
 len_expense = len(expense_report)
 print ("length is %s " % len_expense)
 for i in (range(len_expense)):
-    x = expense_report[i]
-    needed_ans = 2020 - x
+    j =i+1
     for j in range(len_expense):
-        y = expense_report[j]
-        if(y == needed_ans) :
-            product = x*y
-            print(product)
-            break
+        k = j+1
+        for k in range(len_expense):
+            if(expense_report[i]+expense_report[j]+expense_report[k] == 2020):
+                print("answer found")
+                print(expense_report[i]*expense_report[j]*expense_report[k])
+                break
+
 
 
